a28e11d906469f2f79e36369a51c361f88162b22
tomWhilbert <thomaswhilbert@gmail.com>
2025-01-30 11:29:01 -0500

    link to pwsh config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,15 +6,23 @@ New-Alias -Name ga -Value Get-Alias
 
 New-Alias -Name info -Value Get-PnPSite 
 
-
-
-function spcon {
-      param($site)
-      Connect-PnPOnline -Url https://ucblaw.sharepoint.com/sites/"$site" -Interactive
-}
+New-Alias -Name cl -Value clear
 
 New-Alias -Name spexit -Value Disconnect-PnPOnline
 
+New-Alias -Name  exexit disconnect-ExchangeOnline
+
+
+# function spcon {
+    #   param($site)
+    #   Connect-PnPOnline -Url https://ucblaw.sharepoint.com/sites/"$site" -Interactive
+# }
+
+### New Connection methond using registered app and client id
+function spcon {
+    param($site)
+    Connect-PnPOnline -Url https://ucblaw.sharepoint.com/sites/"$site" -Interactive -ClientId 23a59291-9134-42f2-8e9a-f944c6e82529
+}
 
 # Download Site Pages
 function pcopy {
@@ -31,5 +39,3 @@ function pcopy {
 function excon {
      Connect-ExchangeOnline -UserPrincipalName thilbert@clinical.law.berkeley.edu
 }
-
-New-Alias -Name  exexit disconnect-ExchangeOnline
