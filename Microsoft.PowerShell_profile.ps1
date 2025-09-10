cc4c24934b2eb572615595e2128286f687a4d64e
tomWhilbert <thomaswhilbert@gmail.com>
2025-09-10 10:47:16 -0400

    correct restic function - now more functional, haha

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -131,4 +131,4 @@ function New-SecurityGroupPrompt {
 # # Initialize Starship at the end of your profile
 # Invoke-Expression (&starship init powershell)
 
-oh-my-posh init pwsh --config 'amro' | Invoke-Expression 
+oh-my-posh init pwsh --config 'gruvbox' | Invoke-Expression 
